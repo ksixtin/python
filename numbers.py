c7fc7ed70cacda330727e8e6ec3111c067dd7f74
ksixtin <ksixtin@gmail.com>
2019-12-18 16:33:28 +0900

    실습 예제 완료

---
+++ numbers.py
@@ -0,0 +1,18 @@
+# 저장
+
+#1. 숫자
+a = 8
+b = 2
+
+
+print(a)
+
+print(a + b)
+print(a - b)
+print(a * b)
+
+print(a / b)
+print(a // b)       # 나누기 몫만 출력
+print(a % b)        # 나눈후 나머지 값만 출력
+# 짝수 홀수를 판단할때 편함, 값%2를 했을때
+# 값 % 2
